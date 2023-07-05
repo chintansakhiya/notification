eeb55f941cbd117f3f33d641e40a462cc6d891a3
chintan <chintan.sakhiya@improwised.com>
2023-07-05 12:04:37 +0530

    change type

--- notification.go
+++ notification.go
@@ -13,26 +13,20 @@ type Attachment struct {
 	Text  string `json:"text"`
 }
 
-func Notification(err string) {
+func Error(err error, mattermostWebhook string) {
 
 	var m Message
 	var data Attachment
-	// err := os.Args[1]
 
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Please provide two input arguments.")
-	// 	return
-	// }
-
-	if err == "nil" {
+	if err == nil {
 		data.Color = "#B0fc38"
 		data.Text = "Success"
 	} else {
 		data.Color = "#FF0000"
-		data.Text = err
+		data.Text = err.Error()
 	}
 	m.Attachments = append(m.Attachments, data)
 
-	resty.New().R().SetBody(m).Post("http://mattermost.sytes.net/hooks/5bzes4jw5b8dbneepimad4hb4o")
+	resty.New().R().SetBody(m).Post(mattermostWebhook)
 
 }
